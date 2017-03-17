750a301ea50a8431e2829b114deb23882c29bab6
Adam Silverstein <adam@10up.com>
2017-03-17 14:08:18 -0400

    more logging and load interaction

--- includes/js/oauth-demo-widget.js
+++ includes/js/oauth-demo-widget.js
@@ -5,23 +5,34 @@
 ( function( $ ) {
 
 	$( document ).ready( function() {
+		var $container = jQuery( '#submit-oauth-signup' ).parents('.oath-demo-widget'),
+			token = JSON.parse( localStorage.getItem( 'wpOathToken' ) );
+		console.log( token );
+
+		if ( ! token ) {
+			$container.removeClass('loading');
+		} else {
+			$container.addClass( 'loggedin' );
+		}
+
 		$( '#submit-oauth-signup' ).on( 'click', function( e ) {
 			e.preventDefault();
 			console.log( 'Signin!' );
-			var $container   = jQuery( '#submit-oauth-signup' ).parents('.oath-demo-widget'),
-				site         = $container.find( '#oauth-site' ).val(),
+			var site         = $container.find( '#oauth-site' ).val(),
 				clientkey    = $container.find( '#oauth-clientkey' ).val(),
 				clientsecret = $container.find( '#oauth-clientsecret' ).val();
 
 
-				if ( 'undefined' === typeof wp.oauth ) {
-					console.log( 'wp.oauth undfined.' );
-					return;
-				}
+			if ( 'undefined' === typeof wp.oauth ) {
+				console.log( 'wp.oauth undfined.' );
+				return;
+			}
 
-				$container.addClass('loading');
 
-				wp.oauth.connect( clientkey, clientsecret, site );
+
+			$container.addClass('loading');
+
+			wp.oauth.connect( clientkey, clientsecret, site + 'oauth1/request', site + 'oauth1/authorize' );
 
 		} );
 	} );
