46be3999265aadeff124b245451e5e26489e7a95
Adam Silverstein <adam@10up.com>
2017-03-17 11:18:52 -0400

    Widget for oauth, first pass

--- includes/demo-widget.php
+++ includes/demo-widget.php
@@ -1,58 +1,7 @@
 <?php
+namespace wpapioauth\widget;
 
-class OauthDemoWidget extends WP_Widget {
-
-	function __construct() {
-		// Instantiate the parent object
-		parent::__construct( false, 'Oauth Demo Widget' );
-	}
-
-	function widget( $args, $instance ) {
-		// Widget output
-		?>
-		<h3>OAuth Credentials</h3>
-		<p>
-			<div scope="row">
-				<label for="oauth-name">WordPress Site</label>
-			</div>
-			<div>
-				<input type="text" class="regular-text" name="name" id="oauth-name" placeholder="http://yoursite.com" value="">
-			<p class="description">The URL of the WordPress site you want to connect to.</p>
-			</div>
-		</p>
-		<p>
-			<div scope="row">
-				<label for="oauth-clientkey">Client Key</label>
-			</div>
-			<div>
-				<input type="text" class="regular-text" name="name" id="oauth-clientkey" placeholder="" value="">
-			<p class="description"></p>
-			</div>
-		</p>
-		<p>
-			<div scope="row">
-				<label for="oauth-clientsecret">Client Secret</label>
-			</div>
-			<div>
-				<input type="text" class="regular-text" name="name" id="oauth-clientsecret" placeholder="" value="">
-			<p class="description"></p>
-			</div>
-		</p>
-		<p class="submit">
-			<input type="submit" name="submit" id="submit" class="button button-primary" value="Connect"
-		></p>
-						<?php
-	}
-
-	function update( $new_instance, $old_instance ) {
-		// Save widget options
-	}
-
-	function form( $instance ) {
-		// Output admin widget options form
-	}
-}
-
+include 'demo-widget-class.php';
 
 /**
  * Add a demo widget that enables remote login to a WordPress site with basic post editing.
@@ -61,4 +10,14 @@ function oauth_add_demo_widget() {
 	register_widget( 'OauthDemoWidget' );
 
 }
-add_action( 'widgets_init', 'oauth_add_demo_widget' );
+add_action( 'widgets_init', __NAMESPACE__ . '\oauth_add_demo_widget' );
+
+/**
+ * Add some scripts.
+ */
+function oath_add_demo_widget_scripts() {
+	error_log(plugins_url( 'js/oauth-demo-widget.js') );
+	wp_enqueue_script( 'oauth-demo-widget', plugins_url( 'js/oauth-demo-widget.js', __FILE__ ), array( 'jquery', 'wp-api' ) );
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\oath_add_demo_widget_scripts' );
+
